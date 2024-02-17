a290f91563eee747fcf956611f96280fb06b62df
Ashish Madhup <119279720+madhupashish@users.noreply.github.com>
2024-02-17 12:35:42 +0530

    Update config.js

--- databases/config.js
+++ databases/config.js
@@ -1,4 +1,23 @@
 // Database Connection Established
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/socketApp');
+const dbName = 'socketApp';
+const dbUrl = 'mongodb://127.0.0.1:27017/' + dbName;
+
+mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const dbConnection = mongoose.connection;
+
+dbConnection.on('error', (err) => {
+    console.error(`Error connecting to database: ${err}`);
+});
+
+dbConnection.once('open', () => {
+    console.log(`Connected to database: ${dbName}`);
+});
+
+// App Information
+const appName = 'Ashi Chat';
+const developerName = 'Ashish Madhup';
+
+console.log(`${appName} developed by ${developerName}`);
